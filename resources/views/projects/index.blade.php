3701bc47a44f6aa1fbf64ecd9b78fd6b9c3de011
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-13 15:12:16 +0100

    create layout and index/create

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,25 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<x-layout title="Projects">
+
     <h1>Hello</h1>
-    @section('content')
     <h1>Projects</h1>
     <a href="{{ route('projects.create') }}" class="btn btn-primary">Create Project</a>
     <ul>
         @foreach($projects as $project)
             <li>
-                <a href="{{ route('projects.show', $project->id) }}">{{ $project->name }}</a>
+                <strong>{{ $project->name }}</strong>: {{ $project->description }}
+                <a href="{{ route('projects.show', $project->id) }}">View Details</a>
             </li>
         @endforeach
     </ul>
-@endsection
+</x-layout>
 
-
-</body>
-</html>
