cd3352dae448cdea195c958d953773a85ad9b5be
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-16 12:32:33 +0100

    update AuthController with validate() method and then User::create

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // Show FORM
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:5|confirmed',
+        ]);
+
+
+        // create new User with validated data
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'role' => 'user',
+        ]);
+
+        // log User
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 abstract class Controller
 {
-    //
+
 }
