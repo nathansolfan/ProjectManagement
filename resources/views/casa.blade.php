c01ae935d0b4a65f72194e42d9a501be4d7a0083
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-20 15:25:56 +0100

    update

--- resources/views/casa.blade.php
+++ resources/views/casa.blade.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<x-layout title="Welcome to the Home Page">
     <div class="bg-gradient-to-r from-purple-500 to-pink-500 py-20 text-center text-white">
         <h1 class="text-4xl font-bold text-gray-800 mb-8">Welcome to the Home Page</h1>
         <p class="text-2xl max-w-2xl mx-auto">
             This is the home page of your application.
         </p>
     </div>
-</body>
-</html>
-{{-- <x-layout title="Welcome to the Home Page">
-
-</x-layout> --}}
+</x-layout>
